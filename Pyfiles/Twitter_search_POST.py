44b76514bb5cf3f82f74a90c292056a0a8fecd5e
Maria Kleppe <kleppem@users.noreply.github.com>
2014-10-05 20:09:36 -0400

    Update Twitter_search_POST.py

--- Pyfiles/Twitter_search_POST.py
+++ Pyfiles/Twitter_search_POST.py
@@ -1,4 +1,3 @@
-import oauth2
 import time
 import urllib2
 import json
@@ -14,32 +13,43 @@ params["oauth_consumer_key"] = consumer.key
 params["oauth_token_key"] = token.key
 
 
-prev_id = int('<add your start id>')
 
-### this loop stores every 100 tweets in a seperate txt file
-for i in range(100):
-	params["max_id"] = str(prev_id)
-    url = url1
-    req = oauth2.Request(method="GET", url=url, parameters=params) 
-    signature_method = oauth2.SignatureMethod_HMAC_SHA1()
-    req.sign_request(signature_method, consumer, token)
-    headers = req.to_header()
-    url = req.to_url()
-    print url #checkpoint 
-    print headers
-    params["q"] = keyword #add your personal keyword for which you want to get tweets
-    params["count"] = 100 #INSERT INTEGER FROM 1 TO 100
-    params["lang"] = "en"
-    response = urllib2.Request(url)
-    data = json.load(urllib2.urlopen(response))
-    if data["statuses"] == []:
-        print "end of data"
-        break
-    else:
-       	prev_id = int(data["statuses"][-1]["id"]) - 1
-        print prev_id, i  #checkpoint to make sure prev_id changes
-    f = open('<add your filename of choice>' +str(i) +'.txt', 'w')
-    #Example f = open('raw_vegan_' +str(i) +'.txt', 'w')
-    json.dump(data['statuses'], f)
-    f.close()
-    time.sleep(5) #to prevent excess of calls allowed to Twitter API
+
+"""
+This loop stores every 100 tweets in a seperate txt file:
+keyword: add your query term
+id_start: add your id to start your search
+n: how many loops to do
+count: how many tweets per loop
+lang: language
+outfile: name of your file to store tweets
+"""
+def twitter_search(keyword, id_start, n, count, lang, outfile):
+    prev_id = int(id_start)
+    for i in range(start, end):
+        params["max_id"] = str(prev_id)
+        url = url1
+        req = oauth2.Request(method="GET", url=url, parameters=params) 
+        signature_method = oauth2.SignatureMethod_HMAC_SHA1()
+        req.sign_request(signature_method, consumer, token)
+        headers = req.to_header()
+        url = req.to_url()
+        print url #checkpoint 
+        print headers
+        params["q"] = keyword 
+        params["count"] = count 
+        params["lang"] = lang
+        response = urllib2.Request(url)
+        data = json.load(urllib2.urlopen(response))
+        if data["statuses"] == []:
+            print "end of data"
+            break
+        else:
+       	    prev_id = int(data["statuses"][-1]["id"]) - 1
+            print prev_id, i  
+        f = open(outfile +str(i) +'.txt', 'w')
+        json.dump(data['statuses'], f)
+        f.close()
+        time.sleep(5) 
+
+twitter_search(keyword, id_start, start, end, count, lang, outfile)
